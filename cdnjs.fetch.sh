7fcd75df6483b94035f851aea479a9fea3528de3
Peter Dave Hello <hsu@peterdavehello.org>
2016-12-09 01:31:00 +0800

    tuning

--- cdnjs.fetch.sh
+++ cdnjs.fetch.sh
@@ -11,11 +11,11 @@ if [ "${CI}" != "drone" ] && [ "${DRONE}" != "true" ]; then err "Not a Drone CI 
 if [ ! -d ".git" ]; then err "Cache .git directory not found!!! What's going on?"; fi
 if [ -n "${DRONE_PULL_REQUEST}" ]; then DRONE_COMMIT_BRANCH="pull/${DRONE_PULL_REQUEST}/head"; fi
 
-git fetch origin "${DRONE_REPO_BRANCH}"   --depth=50
-git fetch origin "${DRONE_COMMIT_BRANCH}" --depth=50
-
 if echo "${DRONE_REPO_LINK}" | grep 'github.com' > /dev/null 2>&1 ; then
-    wget "${DRONE_REPO_LINK}/raw/${DRONE_COMMIT_SHA}/${PLUGIN_SPARSECHECKOUT}" -O ".git/info/sparse-checkout"
+    wget "${DRONE_REPO_LINK}/raw/${DRONE_COMMIT_SHA}/${PLUGIN_SPARSECHECKOUT}" -O ".git/info/sparse-checkout" &
 else
     err "When does CDNJS drop GitHub? No idea!"
 fi
+
+git fetch origin "${DRONE_REPO_BRANCH}"   --depth=50
+git fetch origin "${DRONE_COMMIT_BRANCH}" --depth=50
