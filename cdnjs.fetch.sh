cdnjs/cdnjs-drone-clone

commit 266b2daa5c59a029bf37ea2d75c045e88484c0df

fetch from origin if fetch from pre-fetch failed

cdnjs.fetch.sh
@@ -26,7 +26,9 @@ else
 fi
 
 if git remote | grep pre-fetch > /dev/null 2>&1 ; then
-    git fetch pre-fetch "${DRONE_REPO_BRANCH}":"${DRONE_REPO_BRANCH}" -f > /dev/null 2>&1
+    git fetch pre-fetch "${DRONE_REPO_BRANCH}":"${DRONE_REPO_BRANCH}" -f > /dev/null 2>&1 || {
+        git fetch origin "${DRONE_COMMIT_BRANCH}" "${DRONE_REPO_BRANCH}":"${DRONE_REPO_BRANCH}" -f
+    }
 else
     git fetch origin "${DRONE_REPO_BRANCH}":"${DRONE_REPO_BRANCH}" -f
 fi

commit 65c224191444becb3b42b9b8bd435060e4131df6

Fix fetch from origin syntax, caused by 266b2daa5c59

cdnjs.fetch.sh
@@ -27,7 +27,7 @@ fi
 
 if git remote | grep pre-fetch > /dev/null 2>&1 ; then
     git fetch pre-fetch "${DRONE_REPO_BRANCH}":"${DRONE_REPO_BRANCH}" -f > /dev/null 2>&1 || {
-        git fetch origin "${DRONE_COMMIT_BRANCH}" "${DRONE_REPO_BRANCH}":"${DRONE_REPO_BRANCH}" -f
+        git fetch origin "${DRONE_REPO_BRANCH}":"${DRONE_REPO_BRANCH}" -f
     }
 else
     git fetch origin "${DRONE_REPO_BRANCH}":"${DRONE_REPO_BRANCH}" -f